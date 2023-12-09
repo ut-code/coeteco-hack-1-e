9283e185f60a2f5c4043b47dd8c6400c1e914272
ts5-25 <104970913+ts5-25@users.noreply.github.com>
2023-12-10 01:55:37 +0900

    最低限の実装

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const API_KEY = '';
+// require('dotenv').config();
+// const API_KEY = process.env.API_KEY;
+const API_KEY = "";
+
 const URL = "https://api.openai.com/v1/chat/completions";
 
-function reply() {
-    var text = document.getElementById("request_text").value;
-    async function getResponse() {
+function askQuestion(theme) {
+    // var text = document.getElementById("request_text").value;
+    const questionsAndAnswers = document.getElementById('questionsAndAnswers');
+    async function makeQuestion() {
         try {
             const response = await axios.post(
                 URL,
                 {
                     "model": "gpt-3.5-turbo",
                     "messages": [
-                        { "role": "user", "content": text }
+                        { "role": "user", "content": `「${theme}」に関する質問を4つ作り、それに回答してください。小学校低学年にわかるように説明してください。` }
                     ]
                 },
                 {
@@ -21,16 +25,45 @@ function reply() {
                 }
             );
             var chatgpt_response = response.data.choices[0].message.content;
-            $("#response_text").val(chatgpt_response);
+            const question = document.createElement('p');
+            question.innerText = chatgpt_response;
+            questionsAndAnswers.appendChild(question);
+            // answerQuestion(chatgpt_response);
         } catch (error) {
             console.log(error);
         }
     }
-    getResponse();
+    /*
+    async function answerQuestion(chatgpt_response) {
+        try {
+            const response = await axios.post(
+                URL,
+                {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [
+                        { "role": "user", "content": `${chatgpt_response} 小学校低学年にわかるように説明してください。` }
+                    ]
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${API_KEY}`,
+                    },
+                }
+            );
+            var chatgpt_response = response.data.choices[0].message.content;
+            const answer = document.createElement('p');
+            answer.innerText = chatgpt_response;
+            questionsAndAnswers.appendChild(answer);
+        } catch (error) {
+            console.log(error);
+        }
+    }*/
+    makeQuestion();
 }
 
 function getKeywords(theme) {
-    var text = document.getElementById("request_text").value;
+    // var text = document.getElementById("request_text").value;
     async function getResponse() {
         try {
             const response = await axios.post(
@@ -38,7 +71,7 @@ function getKeywords(theme) {
                 {
                     "model": "gpt-3.5-turbo",
                     "messages": [
-                        { "role": "user", "content": `「${theme}」に関連する名詞を4つあげてください。4つの名詞はカンマで区切ってください。小学校低学年にわかりやすい言葉をあげてください。「食べ物, 筋肉, 骨, 健康」のように答えてください。` }
+                        { "role": "user", "content": `「${theme}」に関連する名詞を4つあげてください。4つの名詞はカンマ(,)で区切ってください。小学校低学年にわかりやすい言葉をあげてください。「食べ物, 筋肉, 骨, 健康」のように答えてください。` }
                     ]
                 },
                 {
@@ -49,8 +82,8 @@ function getKeywords(theme) {
                 }
             );
             var chatgpt_response = response.data.choices[0].message.content;
-            $("#response_text").val(chatgpt_response);
-            displayKeywordButtons(chatgpt_response.split(','));
+            // $("#response_text").val(chatgpt_response);
+            displayKeywordButtons(theme, chatgpt_response.split(','));
         } catch (error) {
             console.log(error);
         }
@@ -58,7 +91,7 @@ function getKeywords(theme) {
     getResponse();
 }
 
-function displayKeywordButtons(keywords) {
+function displayKeywordButtons(theme, keywords) {
     const keywordButtonsDiv = document.getElementById('keywordButtons');
     keywordButtonsDiv.innerHTML = '';
 
@@ -72,4 +105,26 @@ function displayKeywordButtons(keywords) {
         };
         keywordButtonsDiv.appendChild(button);
     });
+
+    // "についてしつもん" ボタンを表示
+    const questionButtonDiv = document.getElementById('questionButton');
+
+    // すでにボタンが存在する場合は上書き
+    const existingButton = questionButtonDiv.querySelector('button');
+    if (existingButton) {
+        existingButton.innerText = `${theme}についてしつもん`;
+        existingButton.onclick = function() {
+            // ボタンがクリックされたら、そのキーワードに関する質問をChatGPTに送信
+            askQuestion(theme);
+        };
+    } else {
+        // 存在しない場合は新たに作成
+        const button = document.createElement('button');
+        button.innerText = `${theme}についてしつもん`;
+        button.onclick = function() {
+            // ボタンがクリックされたら、そのキーワードに関する質問をChatGPTに送信
+            askQuestion(theme);
+        };
+        questionButtonDiv.appendChild(button);
+    }
 }
